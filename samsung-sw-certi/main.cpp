69059b222089ce419c3dc8292afe92c2580034d9
sne12345 <sne12345@naver.com>
2022-11-07 08:45:48 +0900

    Initial Commit

---
+++ samsung-sw-certi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  samsung-sw-certi
+//
+//  Created by LG on 2022/11/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
